793fa673918de58442a73436a79110988d86af16
Hinako Tanaka <youngbird1228@gmail.com>
2021-09-03 19:28:02 +0900

    ホーム画面の修正

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,14 +82,14 @@
                 <div class="top-right links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ route('posts.create') }}">もくもく新規作成</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
-
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}">Register</a>
                         @endif
                     @endauth
-                    <a href="{{ route('posts.create') }}">もくもく新規作成</a>
+                    
                 </div>
             @endif
 
