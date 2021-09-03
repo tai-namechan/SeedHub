58c3f364d5ed911e7addc21b1864082c08c6699d
Hinako Tanaka <youngbird1228@gmail.com>
2021-09-03 23:27:04 +0900

    seeder作成

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(CategorySeeder::class);
+        $this->call(CategoryTableSeeder::class);
+        $this->call(TimezoneTableSeeder::class);
     }
 }
