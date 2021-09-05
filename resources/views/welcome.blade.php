1c0e6aaddbd3da5a8c6434ecf22c73d28f7daaa5
Taisei Namekawa <s.namety.0106.hta1@gmail.com>
2021-09-05 12:52:37 +0900

    背景画像等コミット

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,84 +1,20 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+    <title>Laravel</title>
 
-        {{-- <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+</head>
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .link > a {
-                color: #636b6f;
-                padding: 0 25px;
-                margin-top: 20px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-            .catch-copy {
-                margin-bottom: 50px;
-            }
-        </style> --}}
-    </head>
-    <body>
-        <div class="welcome wave">
+<body class="welcome-body">
+    <main>
+        <div class="welcome wave ">
             @if (Route::has('login'))
                 <div class="welcome-head">
                     @auth
@@ -118,6 +54,9 @@
             </div>
             <canvas id="waveCanvas"></canvas>
         </div>
-        <script src="{{ asset('js/wave.js') }}" defer></script>
-    </body>
+    </main>
+
+    <script src="{{ asset('js/wave.js') }}" defer></script>
+</body>
+
 </html>
