cde7433286fa182f334c4f1b22117f3da84e3058
Taisei Namekawa <s.namety.0106.hta1@gmail.com>
2022-01-18 02:39:23 +0900

    cssをhttp用

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,7 +9,7 @@
     {{-- <script type="text/javascript" src="//cdn.jsdelivr.net/cal-heatmap/3.3.10/cal-heatmap.min.js"></script> --}}
     {{-- <link rel="stylesheet" href="//cdn.jsdelivr.net/cal-heatmap/3.3.10/cal-heatmap.css" /> --}}
 
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
     <link rel="stylesheet" href="../css/posts/sample.css">
     <title>詳細ページ</title>
 </head>
--- resources/views/users/mypage.blade.php
+++ resources/views/users/mypage.blade.php
@@ -8,7 +8,7 @@
     {{-- <script type="text/javascript" src="//cdn.jsdelivr.net/cal-heatmap/3.3.10/cal-heatmap.min.js"></script> --}}
     {{-- <link rel="stylesheet" href="//cdn.jsdelivr.net/cal-heatmap/3.3.10/cal-heatmap.css" /> --}}
     
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
     <link rel="stylesheet" href="../css/posts/sample.css">
     <title>Profile</title>
 </head>
